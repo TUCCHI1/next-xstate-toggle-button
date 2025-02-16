7fa2ba68e346ee2afb34348edd1cf7a39fea6888
TUCCHI1 <tutikikin1@gmail.com>
2025-02-16 20:13:08 +0900

    chore(eslint): update ESLint configuration to use xstate plugin
    Added xstate plugin to ESLint configuration for improved linting rules related to XState usage.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import xstate from "eslint-plugin-xstate";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,7 +13,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    plugins: ["xstate"],
+    plugins: { xstate },
     rules: {
       "xstate/no-infinite-loop": "error",
       "xstate/no-imperative-action": "error",
